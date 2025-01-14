3560bdbc9b91665dc0db25810254648eac531e6d
Aayush Katoch <101443730+Aayushkatoch@users.noreply.github.com>
2025-01-14 19:27:09 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+ 
+ let score = null
+
+ //console.log(typeof score);
+ //console.log(typeof (score));
+
+
+let ValueInNumber= Number(score)
+ //console.log(ValueInNumber);
+
+
+// "33" >= 33
+// null >= 0
+// true >=1
+// undefined >= NaN (not a number)
+// "33abc" >= NaN
+// false >= 0
+// string >=NaN
+
+let isLoggedIn = 1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn); 
+
+// " " >= false 
+//"Aayush" >= true
+
+let getSomeNo= 50
+let stringGetSomeNo = String(getSomeNo)
+console.log(stringGetSomeNo);
+console.log(typeof stringGetSomeNo);
