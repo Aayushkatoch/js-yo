ac34a40ddbf52c6f0301108328dea7b06d0d1a5f
Aayush Katoch <101443730+Aayushkatoch@users.noreply.github.com>
2025-01-14 18:39:13 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3) //will not work because we are using js not browser
+
+let name = "Aayush"
+let age = 22
+let isLoggedIn = false 
+
+// name is string 
+// age is int 
+// boolean is true or false 
+// null is a type of object or language error
+// undefined is a type 
+
+console.log(typeof true);
